8b6b005c85e2844ed323a531ec10f460f3359aed
Ingo-Rößner <ingo.spicker@gmail.com>
2014-06-12 14:59:07 +0200

    parts know generator

--- generator.h
+++ generator.h
@@ -1,18 +1,42 @@
 #ifndef GENERATOR_H_INCLUDED
 #define GENERATOR_H_INCLUDED
 
-#include "configuration.h"
-#include "Restriction.h"
-
-template<class Conf, class Restrictions, class... Mixins>
-class Generator:public  Mixins...{
-private:
-    static const Restrictions restrictions;
+template<template<class> class... Mixins>
+class Generator:public Mixins<Generator<Mixins...>>...{
 public:
-    Generator():Mixins()...{}
-};
+    Generator():Mixins<Generator<Mixins...>>(*this)...{}
 
-template<class... Mixins>
-using CarGenerator = Generator<Config<Mixins...>, CarRestrictions<Config<Mixins...>>, Mixins...>;
+    template<template<class> class part>
+    static constexpr bool containsPart(){
+        return partEquals<part, Mixins...>();
+    }
+
+    template<class part>
+    static constexpr bool containsDerivedPart(){
+        return partBaseOf<part, Mixins...>();
+    }
+
+private:
+
+    template<template<class> class T1>
+    static constexpr bool partEquals(){
+        return false;
+    }
+
+    template<template<class> class T1, template<class> class T2, template<class> class... Tx>
+    static constexpr bool partEquals(){
+        return is_same<T1<Generator<Mixins...>>, T2<Generator<Mixins...>>>::value ? true : partEquals<T1, Tx...>();
+    }
+
+    template<typename T1>
+    static constexpr bool partBaseOf(){
+        return false;
+    }
+
+    template<typename T1, template<class> class T2, template<class> class... Tx>
+    static constexpr bool partBaseOf(){
+        return is_base_of<T1, T2<Generator<Mixins...>>>::value ? true : partBaseOf<T1, Tx...>();
+    }
+};
 
 #endif // GENERATOR_H_INCLUDED
--- parts.h
+++ parts.h
@@ -2,43 +2,69 @@
 #define PARTS_H_INCLUDED
 
 namespace parts{
+    template<class Generator>
     class Body{
-
+    private:
+        Generator& generator;
+    public:
+        Body(Generator& gen): generator(gen){}
     };
 
     class Engine{
 
     };
 
+    template<class Generator>
     class GasEngine:Engine{
-
+    private:
+        Generator& generator;
+    public:
+        GasEngine(Generator& gen): generator(gen){}
     };
 
+
+    template<class Generator>
     class ElectroEngine:Engine{
-
+    private:
+        Generator& generator;
+    public:
+        ElectroEngine(Generator& gen): generator(gen){}
     };
+
 
     class Transmission{
+
+    };
+
+
+    template<class Generator>
+    class AutomaticTransmission: public Transmission{
+    private:
+        Generator& generator;
     public:
-        static constexpr int gears = 0;
+        AutomaticTransmission(Generator& gen): generator(gen){}
     };
 
-    class AutomaticTransmissionBase: public Transmission{
-    };
 
-    template<int gearCount>
-    class AutomaticTransmission: public AutomaticTransmissionBase{
+    template<class Generator>
+    class ManuelTransmission: Transmission{
+    private:
+        Generator& generator;
     public:
-        static constexpr int gears = gearCount;
+        ManuelTransmission(Generator& gen): generator(gen){}
     };
 
-    class ManuelTransmissionBase: Transmission{
-    };
 
-    template<int gearCount>
-    class ManuelTransmission: ManuelTransmissionBase{
+    template<class Generator>
+    class CarClassification{
+    private:
+        Generator& generator;
     public:
-        static constexpr int gears = gearCount;
+        CarClassification(Generator& gen): generator(gen){
+            static_assert(Generator::template containsPart<parts::Body>(), "a car neads a Body");
+            static_assert(Generator::template containsDerivedPart<parts::Engine>(), "a car neads a Engine");
+            static_assert(Generator::template containsDerivedPart<parts::Transmission>(), "a car neads a Transmission");
+        };
     };
 }
 
